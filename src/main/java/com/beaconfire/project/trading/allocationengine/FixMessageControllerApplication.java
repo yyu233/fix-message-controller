3fee2f8d971b206f1f54d84aafb76471f061d887
Yizheng Yu <yalex778@163.com>
2024-03-23 01:51:58 +0800

    App can launch now

--- src/main/java/com/beaconfire/project/trading/allocationengine/FixMessageControllerApplication.java
+++ src/main/java/com/beaconfire/project/trading/allocationengine/FixMessageControllerApplication.java
@@ -2,8 +2,10 @@ package com.beaconfire.project.trading.allocationengine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.beaconfire.project.trading")
 public class FixMessageControllerApplication {
 
 	public static void main(String[] args) {
